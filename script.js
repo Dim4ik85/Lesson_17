d3171b8727c992484be0bd331b82443d32661979
Dim4ik85 <lafuma4me@gmail.com>
2019-07-18 14:37:58 +0300

    seventeenth commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1 задание
+let secondsInANinute = 60;
+let minutesInAnHour = 60;
+let threeHourInSeconds =3 * secondsInANinute * minutesInAnHour;
+console.log(threeHourInSeconds);
+
+/*2 задание
+let num = 1;
+num = += 12;
+num = -= 14;
+num = *= 5;
+num = /= 7;
+num = ++;
+num = --;
+alert(num);
+*/
+
+//3 задание
+let a = 10, b = 2, c = 5;
+console.log(a + b + c);
+
+//4задание
+let name = "Дима";
+console.log('Hello, '+ name + '!');
